d0d26ae58312ead0c320f3e2bae7d2500b6fa0b6
German Eiter <genryeiter@gmail.com>
2022-02-20 18:17:37 +0200

    Create Dog.java

---
+++ 02_klass/Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+
+    private String dogName = "madrid";
+
+    public void setDogName(String dogName) {
+        this.dogName = dogName;
+    }
+
+    public String getDogName() {
+        return dogName;
+    }
+}
